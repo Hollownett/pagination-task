eb8941214b78440654b0af079d4a21ba151dac33
Hollownett <50251199+Hollownett@users.noreply.github.com>
2020-10-07 15:55:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({
+    people: [],
+  });
+  const [page, setPage] = useState(1);
+  const loader  = useRef(null);
+
+  useEffect(() => {
+    handleDataFromApi("https://swapi.dev/api/films/");
+    console.log(data.people);
+  },[page])
+
+  useEffect(() => {
+    let options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+   };
+  // initialize IntersectionObserver
+  // and attaching to Load More div
+   const observer = new IntersectionObserver(handleObserver, options);
+   if (loader.current) {
+      observer.observe(loader.current)
+   }
+  },[])
+
+ const handleObserver = (entities) => {
+   const target = entities[0];
+   if (target.isIntersecting) {   
+    setPage((page) => page + 1)
+}
+ }
+
+  const handleDataFromApi = (url) => {
+       fetch(url).then(response => {
+        return response.json();
+       }).then(peoples => {
+       const newPeople =  data.people.concat(peoples.results)
+          setData({
+            people: newPeople,
+          })
+       })
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> HI, SWAPI</h1>
+        {data.people.map((item, index) => (
+          <div className="dataContainer" key={index}>
+             {JSON.stringify(item)}
+          </div>
+        ))}
+        <div ref={loader}>
+          <h1>Loadig...</h1>
+        </div>
     </div>
   );
 }
